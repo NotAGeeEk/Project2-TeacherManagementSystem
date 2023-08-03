5ccabaa303a4b391aa644093e8b12b0c969f3f08
Amans <notshashank10@gmail.com>
2023-08-03 09:05:54 +0530

    first commit

---
+++ TeacherFileManager.cs
@@ -0,0 +1,79 @@
+﻿using ConsoleAppTeacherManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class TeacherFileManager
+{
+    private string filename;
+
+    public TeacherFileManager(string filename)
+    {
+        this.filename = filename;
+    }
+
+    public List<Teacher> ReadteachersData()
+    {
+        List<Teacher> teachers = new List<Teacher>();
+
+        try
+        {
+            string[] lines = File.ReadAllLines(filename);
+            bool firstLine = true;
+
+            foreach (string line in lines)
+            {
+                if (firstLine)
+                {
+                    firstLine = false;
+                    continue; // Skip the header line
+                }
+
+                string[] data = line.Split(',');
+                if (data.Length == 4)
+                {
+                    Teacher teacher = new Teacher
+                    {
+                        ID = int.Parse(data[0]),
+                        Name = data[1],
+                        Class = data[2],
+                        Section = data[3]
+                    };
+                    teachers.Add(teacher);
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error reading the file: " + ex.Message);
+        }
+
+        return teachers;
+    }
+
+    public void SaveTeachersData(List<Teacher> teachers)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                writer.WriteLine("ID,Name,Class,Section");
+
+                foreach (var teacher in teachers)
+                {
+                    writer.WriteLine($"{teacher.ID},{teacher.Name},{teacher.Class},{teacher.Section}");
+                }
+            }
+
+            Console.WriteLine("Teacher data saved to file.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error saving the data to file: " + ex.Message);
+        }
+    }
+}
---
+++ bin/Debug/Program.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleAppTeacherManagementSystem;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TeacherFileManager teacherFileManager = new TeacherFileManager("teachersdata.txt");
+
+        List<Teacher> teachers = teacherFileManager.ReadteachersData();
+
+        Console.WriteLine("Teacher Data:");
+        foreach (var teacher in teachers)
+        {
+            Console.WriteLine($"ID: {teacher.ID}, Name: {teacher.Name}, Class: {teacher.Class}, Section: {teacher.Section}");
+        }
+
+        // Example of updating teacher data
+        Console.WriteLine("\nEnter the ID of the teacher you want to update:");
+        int idToUpdate = int.Parse(Console.ReadLine());
+
+        Teacher teacherToUpdate = teachers.Find(t => t.ID == idToUpdate);
+        if (teacherToUpdate != null)
+        {
+            Console.WriteLine("Enter the new name:");
+            teacherToUpdate.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the new class:");
+            teacherToUpdate.Class = Console.ReadLine();
+
+            Console.WriteLine("Enter the new section:");
+            teacherToUpdate.Section = Console.ReadLine();
+
+            Console.WriteLine("Teacher data updated successfully!");
+        }
+        else
+        {
+            Console.WriteLine("Teacher with the given ID not found.");
+        }
+
+        // Save the updated data back to the text file
+        teacherFileManager.SaveTeachersData(teachers);
+
+        Console.ReadKey();
+    }
+}
